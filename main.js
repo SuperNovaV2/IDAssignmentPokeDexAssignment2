309b0704c1ecc11cdf3d18d5b7015575c421d7bf
SuperNovaV2 <s10205505@connect.np.edu.sg>
2021-01-09 22:23:44 +0800

    Add click function for pokedex

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const poke_container = document.getElementById('poke_container');
+
 // Number of Pokemons
-const pokemons_number = 150;
+const pokemons_number = 151;
 // Color coding for different pokemon types
 const colors = {
     fire: '#FDDFDF',
@@ -26,14 +27,14 @@ const fetchPokemon = async () => {
     for (let i=1; i<=pokemons_number; i++){
         await getPokemon(i);
     }
-}
+};
 
 const getPokemon = async id => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const pokemon =  await res.json();
     createPokemonCard(pokemon);
-}
+};
 
 function createPokemonCard(pokemon){
     const pokemonEl = document.createElement("div");
@@ -49,17 +50,79 @@ function createPokemonCard(pokemon){
     pokemonEl.style.backgroundColor = color;
     // I use another api here for images as the images in the original api are not nice
     const pokeInnerHtml =  `
+    <div class = "card" onclick="selectPokemon(${pokemon.id})">
     <div class = "img-container">
         <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${pokeName}" />
     </div>
     <div class = "info">
-        <span class ="number">${pokemon.id.toString().padStart(3,'0')}</span>
+        <span class ="number">#${pokemon.id.toString().padStart(3,'0')}</span>
         <h3 class="name">${pokeName}</h3>
         <small class ="type">Type: <span>${type}</span></small>
     </div>
+    </div>
     `;
-    pokemonEl.innerHTML = pokeInnerHtml
+    pokemonEl.innerHTML = pokeInnerHtml;
     poke_container.appendChild(pokemonEl);
+};
+
+const selectPokemon = async (id) => {
+    await fetchPokemon();
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    displayPopup(pokemon);
+}
+
+const displayPopup = (pokemon) =>{
+    $("#poke_container").empty();
+    const pokeName = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const type = pokemon.types.map((type) =>
+    type.type.name).join(' and ');
+    const ability = pokemon.abilities.map((ability) =>
+    ability.ability.name).join(' and ');
+    const htmlString = `
+    <div class="popup">
+    <div class = "basic-info">
+    <span class ="number">#${pokemon.id.toString().padStart(3,'0')}</span>
+        <h3 class="name">${pokeName}</h3>
+    </div>
+        <button id="closeBtn" onclick="closePopup()">Close</button>
+    <div class = "img-container2">
+        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${pokeName}"/>
+    </div>
+    <div class = "info">
+        <p><small>Type</small></p>   
+        <p><small class ="type"><span>${type}</span></small></p>
+        <p><small>Height: </small>${pokemon.height} | <small>Weight: </small>${pokemon.weight}</p>
+        <p><small>Ability: </small>${ability}
+    </div>
+
+    </div>
+    `
+    var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+    poke_container.innerHTML = htmlString;
+    console.log(pokemon);
 }
 
 fetchPokemon();
+
+
+
