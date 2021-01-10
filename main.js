0f4e7ee454f3c07238f0903162bf00d24578b9b9
SuperNovaV2 <s10205505@connect.np.edu.sg>
2021-01-10 19:13:32 +0800

    Github error again

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const pokemon =  await res.json();
-    createPokemonCard(pokemon);
+     await createPokemonCard(pokemon);
 };
 
 function createPokemonCard(pokemon){
