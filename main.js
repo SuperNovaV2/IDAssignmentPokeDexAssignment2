c2f2a02e583fb23b3f9c4dad53864079e5d04779
SuperNovaV2 <s10205505@connect.np.edu.sg>
2021-01-10 18:13:13 +0800

    Add search bar + Add different regions + Add css

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const poke_container = document.getElementById('poke_container');
 
-// Number of Pokemons
-const pokemons_number = 151;
 // Color coding for different pokemon types
 const colors = {
     fire: '#FDDFDF',
@@ -17,16 +15,81 @@ const colors = {
     psychic: '#ffc9da',
     flying: '#eae3ff',
     fighting: '#E6E0D4',
-    normal: '#F5F5F5'
+    normal: '#F5F5F5',
+    steel: "#e6eaf0",
+    ice: "e8feff",
+    ghost: "dbbaff",
+    dark: "a9abb0"
+
 };
 
 const main_types = Object.keys(colors);
+const region = ($("input[name=rdo]:checked").val());
 
-// To call all 150 pokemon 1 by 1
-const fetchPokemon = async () => {
-    for (let i=1; i<=pokemons_number; i++){
-        await getPokemon(i);
+$("#submit-button1").on("click", function(event){
+    event.preventDefault();
+    const pokemonName = $("#search").val()
+    .trim();
+    $("#poke_container").empty();
+    searchgetPokemon(pokemonName);   
+});      
+
+$("#submit-button").on("click", function(event){
+    event.preventDefault();
+    const region = ($("input[name=rdo]:checked").val());
+    $("#poke_container").empty();
+    fetchPokemon(region);
+});       
+
+// To call pokemon
+const fetchPokemon = async (region) => {
+    if (region == 'kanto'){
+        for (let i=1; i<=151; i++){
+        getPokemon(i);
     }
+}
+    else if (region == 'johto'){
+        for (let i=152; i<=251; i++){
+        getPokemon(i);
+    }
+}
+    else if (region == 'hoenn'){
+        for (let i=252; i<=386; i++){
+        getPokemon(i);
+    }
+}
+    else if (region == 'sinnoh'){
+        for (let i=387; i<=487; i++){
+        getPokemon(i);
+    }
+}
+    else if (region == 'unova'){
+        for (let i=494; i<=649; i++){
+        getPokemon(i);
+    }
+}
+    else if (region == 'kalos'){
+        for (let i=650; i<=718; i++){
+        getPokemon(i);
+    }
+}
+    else if (region == 'alola'){
+        for (let i=722; i<=807; i++){
+        getPokemon(i);
+    }
+}
+    else if (region == 'galar'){
+        for (let i=810; i<=890; i++){
+        getPokemon(i);
+    }
+}
+};
+
+const searchgetPokemon = async (pokemonName) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+    const res = await fetch(url);
+    const pokemon =  await res.json();
+    createPokemonCard(pokemon);
 };
 
 const getPokemon = async (id) => {
@@ -39,7 +102,6 @@ const getPokemon = async (id) => {
 function createPokemonCard(pokemon){
     const pokemonEl = document.createElement("div");
     pokemonEl.classList.add('pokemon');
-
     // Extract the more common/main pokemon type for color coding
     const poke_types = pokemon.types.map(type => type.type.name);
     //This essentially goes over the all the main types ive put above and finds the 1st one from the array in poke_types which is from the api this is to find out the main type the pokemon is
@@ -66,15 +128,15 @@ function createPokemonCard(pokemon){
 };
 
 const selectPokemon = async (id) => {
-    await fetchPokemon();
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const pokemon = await res.json();
     displayPopup(pokemon);
-}
+};
 
-const displayPopup = (pokemon) =>{
+const displayPopup = async (pokemon) =>{
     $("#poke_container").empty();
+    $("#cntr").addClass('hidden');
     const popup = document.createElement("div");
     popup.classList.add('pokemon-popup');
     // Extract the more common/main pokemon type for color coding
@@ -91,6 +153,8 @@ const displayPopup = (pokemon) =>{
     const ability = pokemon.abilities.map((ability) =>
     ability.ability.name).join(' / ')
     .replace(/\b\w/g, (ch) => ch.toUpperCase());
+    const height = (pokemon.height / 10) + "m"
+    const weight = (pokemon.weight / 10) + "kg"
     const pokeInfo = `
     <div class = "basic-info">
     <span class ="number">#${pokemon.id.toString().padStart(3,'0')}</span>
@@ -100,18 +164,17 @@ const displayPopup = (pokemon) =>{
     <div class = "img-container2">
         <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${pokeName}"/>
     </div>
-    <div class = "info">
-        <p><small>Type</small></p>   
-        <p><small class ="type"><span>${typeAll}</span></small></p>
-        <p><small>Height: </small>${pokemon.height} | <small>Weight: </small>${pokemon.weight}</p>
-        <p><small>Ability: </small>${ability}
+    <div class = "info2">
+        <p>Type</p>   
+        <p><span>${typeAll}</span></p>
+        <p>Height: <small>${height}</small> | Weight: <small>${weight}</small></p>
+        <p>Ability: ${ability}</p>
     </div>
     `
     popup.innerHTML = pokeInfo;
     poke_container.appendChild(popup);
     createChart(pokemon);
-    console.log(pokemon);
-}
+};
 
 const createChart = (pokemon) =>{
     var ctx = document.getElementById('myChart').getContext('2d');
@@ -138,16 +201,18 @@ const createChart = (pokemon) =>{
 
 };
 
-fetchPokemon();
-
-const closePopup = (chart) =>{
+const closePopup = () =>{
+    $("#cntr").removeClass('hidden');
     $("#poke_container").empty();
     $("#myChart").remove();
     const newChart = document.createElement("canvas");
     newChart.id = "myChart";
     document.body.appendChild(newChart);
-    fetchPokemon();
+    fetchPokemon(region);
 }
+
+fetchPokemon(region);
+
 
 
 
